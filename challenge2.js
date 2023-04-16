e9a0d3d7cd15e60b3b51ca514754e6fe94b70b70
Utkarshanand15 <utkarshverma689@gmail.com>
2023-04-16 13:20:45 +0530

    challenge2 commit

---
+++ challenge2.js
@@ -0,0 +1,19 @@
+const fs =require("fs");
+//fs.writeFile("Asynchronous.txt","This is a asynchronous file",(err)=>{
+//console.log("File is created ");
+//});
+//fs.appendFile("Asynchronous.txt","\nThis is appended statement",(err)=>{
+  //  console.log("File is appended");
+//});
+//fs.readFile("Asynchronous.txt","utf-8",(err,data)=>{
+  //  console.log(data);
+//});
+//fs.rename("Asynchronous.txt","asynchronous.txt",(err)=>{
+  //  console.log("name change");
+//});
+//fs.unlink("asynchronous.txt",(err)=>{
+  //  console.log("File is deleted");
+//});
+//fs.rmdir("DirName",(err)=>{
+ //   console.log("directory deleted");
+//});
