8142c07b66198a648e88cdc39d644d5e4ddb2f4b
Utkarshanand15 <utkarshverma689@gmail.com>
2023-04-16 12:41:26 +0530

    challenge1 commit

---
+++ challenge.js
@@ -0,0 +1,13 @@
+//***** CHALLENGE 1  {C-create R-Read U-Update D-delete CHALLENGE    }
+ 
+
+const fs=require("fs");
+//fs.writeFileSync("bio.txt","Hello! My name is utkarsh");
+//fs.appendFileSync("bio.txt","\n My roll no is 20051646");
+//const data=fs.readFileSync("bio.txt").toString();
+//console.log(data);
+//fs.renameSync("bio.txt","utkarshbio.txt");
+//***********to delete the file******************* 
+//fs.unlinkSync("utkarshbio.txt");
+//*********** To delete the directory********** */
+//fs.rmdirSync("utkarsh");
